cb311857c0c1830b4a0931d2612ebc7ac2b13f08
Nick <nkyy0923@gmail.com>
2025-03-31 18:13:01 -0700

    added photo chat history upload UI and export chat UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,7 @@
-import { Text, View, SafeAreaView } from "react-native";
+// 应用程序的入口文件，导入并渲染HomePage组件
+import React from 'react';
+import HomePage from './HomePage';
 
 export default function Index() {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  );
+  return <HomePage />;
 }
